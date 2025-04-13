ad4167f344dc0a6d5873317683cc032ed7934b37
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2025-04-13 11:53:35 +0600

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "root";  // Default XAMPP username
-$password = "";  // Default password in XAMPP is empty
+$username = "root";  
+$password = "";  
 $dbname = "student_db";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
